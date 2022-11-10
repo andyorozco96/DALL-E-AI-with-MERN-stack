bc0f32d8e5588ca6d9cf9d7a3731c5327eb5a01a
Andres Orozco <orozco.andres.dev@gmail.com>
2022-11-10 00:05:07 -0300

    (Update) index.js - api config

--- api/index.js
+++ api/index.js
@@ -1,15 +1,40 @@
+/* ===== REQUIRED IMPORTS  ===== */
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
+/* ===== APP INITIALIZATION ===== */
 const app = express();
 
-// Settings
-app.set("port", process.env.PORT || 3001);
+/* ===== VARIABLES ===== */
+const PORT = process.env.PORT || 8082;
+
 // Middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//CORS añadido
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+
+  next();
+});
 
 // Routes
 
 // Static files
 
-// Starting the server
-app.listen(3001, () => {
-  console.log("server on port 3001");
+/* ===== APP LISTENING ===== */
+
+app.listen(PORT, () => {
+  console.log(`server on port ${PORT}`);
 });
